16422de2ac664be96f764d911b20ff7fb86cf1ee
Senzo Maseko <senzo.e.maseko@gmail.com>
2022-11-18 00:38:16 +0200

    Initial commit

--- blognificent/settings.py
+++ blognificent/settings.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
