c83e4ab7d5af4425bba66e63f11d81bda0560f20
Senzo Maseko <senzo.e.maseko@gmail.com>
2022-11-24 15:23:52 +0200

    Textarea resizing

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+import crispy_forms.helper
 from django import forms
 
 from blog.models import Comment
@@ -7,10 +8,13 @@ class EmailForm(forms.Form):
     name = forms.CharField(max_length=30)
     email = forms.EmailField()
     to = forms.EmailField()
-    comment = forms.CharField(required=False, widget=forms.Textarea)
+    comment = forms.CharField(required=False, widget=forms.Textarea(attrs={'cols': 20, 'rows': 10}))
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'cols': 20, 'rows': 10})
+        }
